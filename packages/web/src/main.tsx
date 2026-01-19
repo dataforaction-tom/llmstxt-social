87d0f40157a7f04d13dbbd29f531cfb16ea9121c
Tom C Watson <tom.watson@dataforaction.org.uk>
2026-01-19 09:26:36 +0000

    Revert "Add Plausible tracker init"
    This reverts commit f13ca20cd9b28b0d8fec34faf7cdaa649eb72944.

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { init } from '@plausible-analytics/tracker';
 import App from './App.tsx';
 import './index.css';
-
-if (typeof window !== 'undefined') {
-  init({ domain: 'llmstxt.social' });
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
